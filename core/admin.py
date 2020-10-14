f76547ad658549855beeb790beb12487b9acdb5e
slsmoraes <slsmoraes@gmail.com>
2020-10-14 19:43:06 -0300

    D20201014H1942-Atualizando o projeto agenda no Github

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento2
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao', 'usuario')
+    list_filter = ('usuario', 'data_evento',)
+
+admin.site.register(Evento2, EventoAdmin)
